f8a8046179718dde46b0b6b90b2adf2b51b5042b
songyikim0911 <ksi6470@gmail.com>
2021-08-16 18:59:48 +0900

    a

--- src/main/java/com/song/anypractice2108/note/NoteService.java
+++ src/main/java/com/song/anypractice2108/note/NoteService.java
@@ -1,4 +1,29 @@
 package com.song.anypractice2108.note;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class NoteService {
+
+    private Map<String, ArrayList<NoteDTO>> noteMap = new HashMap<>();
+    private Integer idx = 0;
+
+    public Integer add(NoteDTO noteDTO){
+        noteDTO.setNo(++idx);
+        String owner = noteDTO.getWhom();
+
+        ArrayList<NoteDTO> noteDTOS = noteMap.get(owner);
+
+        if(noteDTOS == null){
+            ArrayList<NoteDTO> noteList = new ArrayList<>();
+            noteList.add(noteDTO);//noteList에 값만 넣었는데 noteMap으로 연동..?어떻게?
+        }
+        return idx;
+    }
+
+    public ArrayList<NoteDTO> getList(String owner){
+        return noteMap.get(owner);
+    }
 }
--- src/main/java/com/song/anypractice2108/note/NoteTest.java
+++ src/main/java/com/song/anypractice2108/note/NoteTest.java
@@ -1,4 +1,20 @@
 package com.song.anypractice2108.note;
 
+import com.google.gson.Gson;
+
 public class NoteTest {
+    public static void main(String[] args) {
+
+        NoteDTO data = NoteDTO.builder().who("A").whom("B").content("커피한잔?").build();
+
+        Command command = Command.builder().oper("ADD").noteDTO(data).build();
+
+        System.out.println(command);
+
+        Gson gson = new Gson();
+
+        System.out.println(gson.toJson(command));
+
+    }
+
 }
