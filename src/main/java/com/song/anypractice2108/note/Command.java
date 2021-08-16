c343a20a1fb208acb61d252406b181591c730254
songyikim0911 <ksi6470@gmail.com>
2021-08-16 17:56:35 +0900

    a

--- src/main/java/com/song/anypractice2108/note/Command.java
+++ src/main/java/com/song/anypractice2108/note/Command.java
@@ -1,4 +1,19 @@
 package com.song.anypractice2108.note;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class Command {
+
+    private String oper;
+
+    private NoteDTO noteDTO;
+
 }
--- src/main/java/com/song/anypractice2108/note/NoteMapTest.java
+++ src/main/java/com/song/anypractice2108/note/NoteMapTest.java
@@ -1,4 +1,35 @@
 package com.song.anypractice2108.note;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class NoteMapTest {
+    public static void main(String[] args) {
+
+        Map<String, ArrayList<NoteDTO>> noteMap = new HashMap<>();
+
+        System.out.println(noteMap+"1");
+        String who = "user1";
+        NoteDTO newbie = NoteDTO.builder().who("user2").whom("user1").content("커피한잔?").build();
+
+        System.out.println(noteMap+"2");
+        ArrayList<NoteDTO> noteDTOS = noteMap.get(who);//값만 꺼내는거... 왜 why! m2에서는 이때 연결되는 이유가 뭐지?
+
+        System.out.println(noteMap+"3");
+        if(noteDTOS == null){
+            ArrayList<NoteDTO> noteList = new ArrayList<>();
+            noteList.add(newbie);
+            System.out.println(noteMap+"4-1");
+            noteMap.put(who,noteList);
+
+            System.out.println(noteMap+"4-2");//값이 들어가는 지점!!!
+        }else{
+            noteDTOS.add(newbie);
+
+            System.out.println(noteMap+"5");
+        }
+
+    }
+
 }
